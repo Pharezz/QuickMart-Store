fe832792a9bfee3daf3b132b0c98444feb0e543f
Pharezz <oguegbuikechukwu@gmail.com>
2025-08-27 22:57:15 +0100

    update2

--- app/search/page.js
+++ app/search/page.js
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import SearchContent from "./SearchContent";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading search page...</div>}>

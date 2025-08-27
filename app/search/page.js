074c688d931546acd52489f4b40ee635a5884712
Pharezz <oguegbuikechukwu@gmail.com>
2025-08-27 23:19:00 +0100

    y

--- app/search/page.js
+++ app/search/page.js
@@ -1,7 +1,9 @@
 import { Suspense } from "react";
-import SearchContent from "./SearchContent";
+import SearchContent from "./SearchContent.client";
 
 export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+
 
 export default function Page() {
   return (
